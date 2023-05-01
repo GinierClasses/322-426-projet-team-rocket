07ee398592b05f25f44aa70fe763f7fb1ec1886b
ColinRgm <90181373+ColinRgm@users.noreply.github.com>
2023-05-01 12:35:13 +0200

    Initialisation of the "grades-table-component"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { GradesTableComponent } from './components/grades-table/grades-table.component';
 
 @NgModule({
-  declarations: [AppComponent, SidebarComponent],
+  declarations: [AppComponent, SidebarComponent, GradesTableComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/components/grades-table/grades-table.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-grades-table',
+  templateUrl: './grades-table.component.html',
+  styleUrls: ['./grades-table.component.scss']
+})
+export class GradesTableComponent {
+
+}
