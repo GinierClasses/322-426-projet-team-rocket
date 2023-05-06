1ad9da18a22f13787cb1673132291de4edb808d0
JorisBly <joris.bailly@jobtrek.ch>
2023-05-06 12:29:48 +0200

    feat(): Add get for json server to get all modules

--- src/app/services/modules.service.ts
+++ src/app/services/modules.service.ts
@@ -1,11 +1,22 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import {ModuleModel} from "../models/module.model";
 
+@Injectable({
+  providedIn: 'root'
+})
 export class ModulesService{
 
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
 
-  getModules() {
-return null;
+  constructor(private http: HttpClient) { }
+
+
+
+  getModules(): Observable<any> {
+    const url = 'http://localhost:3000/modules'; // URL du serveur JSON
+
+    return this.http.get<any>(url);
   }
 
 }
