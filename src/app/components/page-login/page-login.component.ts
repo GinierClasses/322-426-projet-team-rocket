bba58ae23d57637b9368ea6416a6c2bcf9a31679
BastienVienet <bastien.vienet@outlook.com>
2023-05-07 23:52:16 +0200

    Improving the regex and the error message

--- src/app/components/page-login/page-login.component.ts
+++ src/app/components/page-login/page-login.component.ts
@@ -13,13 +13,16 @@ export class PageLoginComponent {
   ]);
   password = new FormControl('', [
     Validators.required,
-    // At least one uppercase letter ((?=.*[A-Z]))
-    // At least one lowercase letter ((?=.*[a-z]))
-    // At least one special character ((?=.*[!@#$%^&*()_+\-\=[\]{};':"\\|,.<>\/?]))
-    // At least 8 characters long (.{8,})
-    Validators.pattern(
-      /^(?=.*[A-Z])(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?])(?=.*[a-z]).{8,}$/
-    ),
+    // One uppercase
+    Validators.pattern(/(?=.*[A-Z])/),
+    // One lowercase
+    Validators.pattern(/(?=.*[a-z])/),
+    // One number
+    Validators.pattern(/(?=.*[0-9])/),
+    // One special character
+    Validators.pattern(/(?=.*[!@#$%^&*])/),
+    // At least 8 characters
+    Validators.pattern(/(?=.{8,})/),
   ]);
 
   getEmailErrorMessage() {
@@ -36,11 +39,44 @@ export class PageLoginComponent {
 
   getPasswordErrorMessage() {
     if (this.password.hasError('required')) {
-      return 'Vous devez insérez un mot de passe';
+      return 'Vous devez insérer un mot de passe';
     }
 
-    if (this.password.hasError('pattern')) {
-      return 'Votre mot de passe doit contenir au moins 8 caractères, une majuscule et un caractère spécial';
+    const passwordValue = this.password.value ?? '';
+    const errors = [];
+
+    if (!/(?=.*[A-Z])/.test(passwordValue)) {
+      errors.push('une majuscule');
+    }
+
+    if (!/(?=.*[a-z])/.test(passwordValue)) {
+      errors.push('une minuscule');
+    }
+
+    if (!/(?=.*\d)/.test(passwordValue)) {
+      errors.push('un chiffre');
+    }
+
+    if (!/(?=.*[!@#$%^&*])/i.test(passwordValue)) {
+      errors.push('un caractère spécial');
+    }
+
+    if (!/(?=.{8,})/.test(passwordValue)) {
+      errors.push('au moins 8 caractères');
+    }
+
+    if (errors.length > 0) {
+      let errorMessage = 'Votre mot de passe doit contenir également ';
+      if (errors.length === 1) {
+        errorMessage += errors[0];
+      } else if (errors.length === 2) {
+        errorMessage += `${errors[0]} et ${errors[1]}`;
+      } else {
+        errorMessage += `${errors.slice(0, errors.length - 1).join(', ')} et ${
+          errors[errors.length - 1]
+        }`;
+      }
+      return errorMessage;
     }
 
     return '';
