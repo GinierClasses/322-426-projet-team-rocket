c9d570a0cb1e2182d632b976843d4c03d42bfbfa
De Coster Keanu <dckeanu@hotmail.com>
2023-05-09 19:13:33 +0200

    Title box container

---
+++ src/app/components/title-box-container/title-box-container.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-title-box-container',
+  templateUrl: './title-box-container.component.html',
+  styleUrls: ['./title-box-container.component.scss']
+})
+export class TitleBoxContainerComponent {
+
+}
