1290949301a16c4d4ec0ebc04c369ec86b87e533
De Coster Keanu <dckeanu@hotmail.com>
2023-05-10 09:09:53 +0200

    Added root and fixe html and css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {StepperComponent} from "./components/stepper/stepper.component";
+import {PonderationComponent} from "./components/ponderation/ponderation.component";
 
-const routes: Routes = [{ path: 'stepper', component: StepperComponent }];
+const routes: Routes = [
+  {path: 'stepper', component: StepperComponent},
+  {path: 'ponderations', component: PonderationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
