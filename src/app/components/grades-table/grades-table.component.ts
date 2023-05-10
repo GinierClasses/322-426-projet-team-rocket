123eb4d169cd62aa58980afd15a0c9d298290a6b
ColinRgm <90181373+ColinRgm@users.noreply.github.com>
2023-05-10 09:10:13 +0200

    work in progress

--- src/app/components/grades-table/grades-table.component.ts
+++ src/app/components/grades-table/grades-table.component.ts
@@ -6,9 +6,17 @@ export interface coursEPSIC {
 }
 
 const ELEMENT_DATA: coursEPSIC[] = [
-  {name: 'i114', note: 4},
-  {name: 'i254', note: 5.5},
-  {name: 'i296', note: 6}
+  {name: 'Module 165 - Utiliser des bases de données NoSQL', note: 5},
+  {name: 'Module 254 - Décrire des processus métier dans son propre environnement professionnel', note: 5},
+  {name: 'Module 322 - Concevoir et implémenter des interfaces utilisateur', note: 5},
+  {name: 'Module 346 - Concevoir et réaliser des solutions cloud', note: 5},
+  {name: 'Module 426 - Développer un logiciel avec des méthodes agiles', note: 5},
+  {name: 'Module 114 - Mettre en œuvre des systèmes de codification, de compression et d\'encryptage', note: 5},
+  {name: 'Module 320 - Programmation orientée objet', note: 5},
+  {name: 'Module 347 - Utiliser un service avec des conteneurs', note: 5},
+  {name: 'Anglais', note: 5},
+  {name: 'Math', note: 5},
+  {name: 'ECG', note: 5},
 ];
 
 @Component({
