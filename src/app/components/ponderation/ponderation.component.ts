c05d04682a37bd5cadd558c55aa3568d5316a398
De Coster Keanu <dckeanu@hotmail.com>
2023-05-10 09:59:20 +0200

    Added dynamic title

--- src/app/components/ponderation/ponderation.component.ts
+++ src/app/components/ponderation/ponderation.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-ponderation',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./ponderation.component.scss']
 })
 export class PonderationComponent {
-
+  title = 'Pondérations';
 }
