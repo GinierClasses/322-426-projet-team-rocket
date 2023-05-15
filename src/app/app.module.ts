faadf40348b20824390d010a516aefa78d5d3212
bryan.heutschi <bheutschi@gmail.com>
2023-05-15 13:27:47 +0200

    add my component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { GradesTableComponent } from './components/grades-table/grades-table.component';
+import { StepperComponent } from './components/stepper/stepper.component';
+import {MatStepperModule} from "@angular/material/stepper";
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
-  declarations: [AppComponent, SidebarComponent, GradesTableComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  declarations: [AppComponent, SidebarComponent, StepperComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatStepperModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
