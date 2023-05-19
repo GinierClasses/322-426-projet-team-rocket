35c9efc4b70fcf451aa714bb08d3a2092b82b07d
bryan.heutschi <bheutschi@gmail.com>
2023-05-19 12:35:08 +0200

    Add stepper for add grade and for edit grade

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {StepperComponent} from "./components/stepper/stepper.component";
 import {GradesTablePageComponent} from "./components/grades-table-page/grades-table-page.component";
+import {ChangeGradeComponent} from "./components/change-grade/change-grade.component";
 
 const routes: Routes = [
   {path: 'stepper', component: StepperComponent},
   {path: 'grades', component: GradesTablePageComponent},
-  {path: 'profil', component: ProfilPageComponent}];
+  {path: 'profil', component: ProfilPageComponent},
+  {path: 'change', component: ChangeGradeComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
