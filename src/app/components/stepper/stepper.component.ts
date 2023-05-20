db661daf07a1acde412772c99465e9a2f7fb5c7c
JorisBly <joris.bailly@jobtrek.ch>
2023-05-20 11:08:23 +0200

    Add back for the selelct in stepper

--- src/app/components/stepper/stepper.component.ts
+++ src/app/components/stepper/stepper.component.ts
@@ -1,38 +1,35 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ModulesService } from 'src/app/services/modules.service';
 import { ModuleModel } from 'src/app/models/module.model';
+import { MatStepper } from '@angular/material/stepper';
 
 @Component({
   selector: 'app-stepper',
   templateUrl: './stepper.component.html',
   styleUrls: ['./stepper.component.scss']
 })
-
 export class StepperComponent implements OnInit {
 
   title = "Ajouter une note";
-
   isLinear = false;
   modules: ModuleModel[] = [];
-  
+  selectedModule: ModuleModel | undefined;
+  selectedGrade: string | undefined;
+  @ViewChild('stepper') stepper: MatStepper | undefined;
 
   constructor(
     private _formBuilder: FormBuilder,
     private moduleService: ModulesService
-){}
-  
-  
+  ) {}
 
-    firstFormGroup = this._formBuilder.group({
-      firstCtrl: ['', Validators.required],
-    });
-    secondFormGroup = this._formBuilder.group({
-      secondCtrl: ['', Validators.required],
-    });
-  
+  firstFormGroup: FormGroup = this._formBuilder.group({
+    selectedModule: ['', Validators.required],
+  });
 
-  
+  secondFormGroup: FormGroup = this._formBuilder.group({
+    selectedGrade: ['', Validators.required],
+  });
 
   ngOnInit() {
     this.moduleService.getModules().subscribe((modules: ModuleModel[]) => {
@@ -40,5 +37,26 @@ export class StepperComponent implements OnInit {
     });
   }
 
-}
+  goToNextStep() {
+    if (this.stepper) {
+      this.stepper.next();
+    }
+  }
 
+  onModuleSelectionChange(event: any) {
+    this.selectedModule = event.value;
+  }
+
+  onGradeSelectionChange(event: any) {
+    this.selectedGrade = event.value;
+  }
+
+  onSubmit() {
+    const moduleSelectionne = this.selectedModule;
+    const noteSelectionnee = this.selectedGrade;
+    if (moduleSelectionne && noteSelectionnee) {
+      const phrase = `Vous allez ajouter la note ${noteSelectionnee} au module ${moduleSelectionne.name} - ${moduleSelectionne.description}.`;
+      console.log(phrase);
+    }
+  }
+}
