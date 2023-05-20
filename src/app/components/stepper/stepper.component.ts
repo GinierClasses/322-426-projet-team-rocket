d8907db61a4c2268209d38f525357f618e473a08
JorisBly <joris.bailly@jobtrek.ch>
2023-05-20 11:00:30 +0200

    Add back for the selelct in stepper

--- src/app/components/stepper/stepper.component.ts
+++ src/app/components/stepper/stepper.component.ts
@@ -1,26 +1,44 @@
-import {Component} from '@angular/core';
-import {FormBuilder, Validators} from '@angular/forms';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { ModulesService } from 'src/app/services/modules.service';
+import { ModuleModel } from 'src/app/models/module.model';
 
 @Component({
   selector: 'app-stepper',
   templateUrl: './stepper.component.html',
   styleUrls: ['./stepper.component.scss']
 })
-export class StepperComponent {
+
+export class StepperComponent implements OnInit {
 
   title = "Ajouter une note";
 
-
-
-
-  firstFormGroup = this._formBuilder.group({
-    firstCtrl: ['', Validators.required],
-  });
-  secondFormGroup = this._formBuilder.group({
-    secondCtrl: ['', Validators.required],
-  });
   isLinear = false;
+  modules: ModuleModel[] = [];
+  
 
-  constructor(private _formBuilder: FormBuilder) {
+  constructor(
+    private _formBuilder: FormBuilder,
+    private moduleService: ModulesService
+){}
+  
+  
+
+    firstFormGroup = this._formBuilder.group({
+      firstCtrl: ['', Validators.required],
+    });
+    secondFormGroup = this._formBuilder.group({
+      secondCtrl: ['', Validators.required],
+    });
+  
+
+  
+
+  ngOnInit() {
+    this.moduleService.getModules().subscribe((modules: ModuleModel[]) => {
+      this.modules = modules;
+    });
   }
+
 }
+
