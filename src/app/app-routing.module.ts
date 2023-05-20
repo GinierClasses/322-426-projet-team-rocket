101667f79320389c5b98c4c6cc3ff7966133bf1a
De Coster Keanu <dckeanu@hotmail.com>
2023-05-20 18:18:36 +0200

    renamed root and import for change-grade and add-grade

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,21 @@
 import {ProfilPageComponent} from './components/profil-page/profil-page.component';
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {StepperComponent} from "./components/stepper/stepper.component";
 import {GradesTablePageComponent} from "./components/grades-table-page/grades-table-page.component";
 import {ChangeGradeComponent} from "./components/change-grade/change-grade.component";
 import {PonderationComponent} from "./components/ponderation/ponderation.component";
 import {PageDashboardComponent} from "./components/page-dashboard/page-dashboard.component";
 import {ReportProblemComponent} from "./components/report-problem/report-problem.component";
+import {AddGradeComponent} from "./components/add-grade/add-grade.component";
 
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: 'dashboard'},
   {path: 'ponderations', component: PonderationComponent},
-  {path: 'stepper', component: StepperComponent},
+  {path: 'add-grade', component: AddGradeComponent},
   {path: 'grades', component: GradesTablePageComponent},
   {path: 'profil', component: ProfilPageComponent},
-  {path: 'change', component: ChangeGradeComponent},
+  {path: 'change-grade', component: ChangeGradeComponent},
   {path: 'dashboard', component: PageDashboardComponent},
   {path: 'problem', component: ReportProblemComponent},
   {path: '**', component: PageDashboardComponent}
