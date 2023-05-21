97e112dc14f169e87b256f21e26a1028fb8cb0a7
BastienVienet <bastien.vienet@outlook.com>
2023-05-21 18:57:07 +0200

    Fake login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { FormControl, Validators } from '@angular/forms';
 
 @Component({
@@ -12,6 +13,13 @@ export class LoginComponent {
     Validators.pattern(/^[A-Za-z0-9._%+-]+@eduvaud\.ch$/),
   ]);
 
+  password = new FormControl('', [
+    Validators.required,
+    Validators.pattern(/^Plagi@t!$/),
+  ]);
+
+  constructor(private router: Router) {}
+
   getEmailErrorMessage() {
     if (this.email.hasError('required')) {
       return 'Vous devez insérez une adresse email';
@@ -22,5 +30,29 @@ export class LoginComponent {
     }
 
     return '';
+  }
+
+  getPasswordErrorMessage() {
+    if (this.password.hasError('required')) {
+      return 'Vous devez insérer un mot de passe';
+    }
+
+    if (this.password.hasError('pattern')) {
+      return 'Le mot de passe doit être "Plagi@t!"';
+    }
+
+    return '';
+  }
+
+  login() {
+    const fakeUserEmail = 'flavio.pacifico@eduvaud.ch';
+    const enteredEmail = this.email.value;
+    const enteredPassword = this.password.value;
+
+    if (enteredEmail === fakeUserEmail && enteredPassword === 'Plagi@t!') {
+      this.router.navigate(['/dashboard']);
+    } else {
+      alert('Email ou mot de passe incorrect');
+    }
   }
 }
